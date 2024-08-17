51a0a170868966ceec4c30265807483042e6dcb8
naiko <3443471283@qq.com>
2024-08-17 23:17:13 +0800

    暂时这样

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -1,8 +1,20 @@
 "use client"
 
+import { Dispatch, SetStateAction,createContext} from "react"
+
 type State = {
+    // 是否显示导航栏
     displayNavigation:boolean
 }
 type AppContextProps= {
-    
+    // 包含一个状态类型
+    State:State
+    // 状态的更新函数
+    setSate:Dispatch<SetStateAction<State>>
+}
+const AppContext = createContext<AppContextProps>(null!);
+
+
+export default function AppContextProps(){
+
 }
