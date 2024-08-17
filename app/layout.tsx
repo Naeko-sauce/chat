1cfc66d810d12d7cb24df673681c63fee1ac9f99
naiko <3443471283@qq.com>
2024-08-17 21:00:24 +0800

    添加黑夜模式

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-red-500 p-10">{children}</body>
+      <body className="dark">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+import Navigation from "@/components/home/Navigation";
+import Main from "@/components/home/main";
 
 export default function Home() {
+ 
   return (
-    <main className="bg-yellow-500 p-10">
-      <h1>halo</h1>
-    </main>
+    <div className="flex h-full">
+      
+      <Navigation />
+      <Main  />
+      
+
+    </div>
   );
 }
+ 
