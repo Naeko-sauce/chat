608699b40161b75b738a2b6c4762be74211aad3c
naiko <3443471283@qq.com>
2024-08-26 07:34:57 +0800

    暂定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import AppContextProvider from "@/components/AppContext"
-import "./globals.css";
+import "../style/globals.css";
+import "../style/Markdown.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
--- components/common/Markdown.tsx
+++ components/common/Markdown.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react"
 import ReactMarkdown, { Options } from "react-markdown"
-function Markdown({children,...props}:Options){
-    return <ReactMarkdown {...props}>{children}</ReactMarkdown>
+function Markdown({children,className="",...props}:Options){
+    return <ReactMarkdown className={` markdown prose dark:prose-invert ${className}`} {...props}>{children}</ReactMarkdown>
 }
 export default memo (Markdown)
