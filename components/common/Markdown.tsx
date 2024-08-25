ef85e6a9a92d6c944a76603f701dba3df62e0c46
naiko <3443471283@qq.com>
2024-08-26 07:18:52 +0800

    导入css

---
+++ components/common/Markdown.tsx
@@ -0,0 +1,6 @@
+import { memo } from "react"
+import ReactMarkdown, { Options } from "react-markdown"
+function Markdown({children,...props}:Options){
+    return <ReactMarkdown {...props}>{children}</ReactMarkdown>
+}
+export default memo (Markdown)
--- components/home/Main/idex.tsx
+++ components/home/Main/idex.tsx
@@ -1,5 +1,6 @@
 import ChatInput from "./ChatInput";
 import Menu from "./Menu";
+import MessageList from "./Message";
 import ModelSelect from "./ModelSelect";
 import Welcome from "./Welcome";
 
@@ -7,7 +8,8 @@ export default function Main (){
     return <div className="relative flex-1">
      <main className=" overflow-y-auto  bg-white w-full h-full text-gray-900 dark:bg-gray-800 dark:text-gray-100">
         <Menu/>
-        <Welcome/>
+        {/* <Welcome/> */}
        <ChatInput/>
+       <MessageList />
         </main></div>
 }
